b15ab25e59bfb9f2c7ea4395f2a03bf4beec798f
marnezi <59050480+marnezi@users.noreply.github.com>
2025-08-21 13:07:18 +0100

    Update Norm_shapes.py

--- Norm_shapes.py
+++ Norm_shapes.py
@@ -21,7 +21,7 @@ def clean_axes(ax):
     ax.annotate("", xy=(0, -1.6), xytext=(0, 0),
                 arrowprops=dict(arrowstyle="->", lw=1.2, color="black"))
 
-#ℓ0.5
+#l 0.5
 t = np.linspace(0, 2*np.pi, 400)
 c, s = np.cos(t), np.sin(t)
 r = (np.abs(c)**0.5 + np.abs(s)**0.5)**(-1/0.5)
@@ -32,7 +32,7 @@ ax0.set_title(r"$p=0.5$", fontsize=16, pad=6)
 ax0.text(1, -1.45, r"$\|x\|_{0.5}=(\sum {\sqrt{|x_i|}})^2$", ha="center", fontsize=14)
 
 
-#ℓ1 
+#l 1 
 t = np.linspace(0, 2*np.pi, 400)
 c, s = np.cos(t), np.sin(t)
 r = (np.abs(c)**1 + np.abs(s)**1)**(-1/1)
@@ -42,7 +42,7 @@ clean_axes(ax1)
 ax1.set_title(r"$p=1$", fontsize=16, pad=6)
 ax1.text(0.8, -1.45, r"$\|x\|_{1}=\sum |x_i|$", ha="center", fontsize=14)
 
-#  ℓ2 
+#l 2 
 r = (np.abs(c)**2 + np.abs(s)**2)**(-1/2)
 x2, y2 = r*c, r*s
 ax2.plot(x2, y2, color="#3776ab", lw=2)
@@ -50,7 +50,7 @@ clean_axes(ax2)
 ax2.set_title(r"$p=2$", fontsize=16, pad=6)
 ax2.text(0.8, -1.45, r"$\|x\|_{2}=\sqrt{\sum x_i^{2}}$", ha="center", fontsize=14)
 
-# ℓ∞ inf
+#l inf
 x3 = np.array([-1, 1, 1, -1, -1])
 y3 = np.array([-1,-1, 1,  1, -1])
 ax3.plot(x3, y3, color="#3776ab", lw=2)
@@ -61,3 +61,4 @@ ax3.text(0.8, -1.45, r"$\|x\|_{\infty}=\max |x_i|$", ha="center", fontsize=14)
 plt.suptitle(r"$L_p$ norms", fontsize=20, y=0.98)
 plt.tight_layout(rect=[0, 0, 1, 0.95])
 plt.show()
+
